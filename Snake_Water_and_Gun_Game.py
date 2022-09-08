0c01661d4a06bc4d80420c96d4492ce6f6ec6b20
SukhadJoshi <sukhad2002@gmail.com>
2022-09-08 13:32:01 +0530

    Initial commit

---
+++ Snake_Water_and_Gun_Game.py
@@ -0,0 +1,45 @@
+import random
+def game(comp, you):
+
+    if comp == you:
+        return None
+
+    elif comp == 's':
+        if you=='w':
+            return False
+        elif you=='g':
+            return True
+    
+    elif comp == 'w':
+        if you=='g':
+            return False
+        elif you=='s':
+            return True
+    
+    elif comp == 'g':
+        if you=='s':
+            return False
+        elif you=='w':
+            return True
+
+print("Comp Turn: Snake(s) Water(w) or Gun(g)")
+ran = random.randint(1, 3) 
+if ran == 1:
+    comp = 's'
+elif ran == 2:
+    comp = 'w'
+elif ran == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s) Water(w) or Gun(g)?-")
+a = game(comp, you)
+
+print(f"Computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
